b9b4d9dcb319e1e46594724568dfa8227be4022b
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-11 20:40:39 -0400

    created outline of project

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,23 @@
 public class Student {
+    private String FirstName;
+    private String LastName;
+    private String graduateYear;
+    private int StudentID;
+    private String courses;
+    private int Balance = 0;
+    private int course_cost = 600;
+
+    // Constructor to ask user to enter name of student and graduate year
+
+    // Generate an student ID
+
+    // Enroll for courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
 
 }
