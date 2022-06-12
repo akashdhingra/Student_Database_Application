deb566b869b95b546b25281f762d9f8fed7d8284
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-11 20:57:11 -0400

    implemented show info method

--- src/Student.java
+++ src/Student.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Student {
     private String FirstName;
     private String LastName;
-    private String graduateYear;
+    private int graduateYear;
     private int StudentID;
     private String courses;
     private int Balance = 0;
     private int course_cost = 600;
+    private int id = 1000;
 
     // Constructor to ask user to enter name of student and graduate year
     public Student()
@@ -18,10 +19,18 @@ public class Student {
         System.out.println("Enter your Last Name");
         this.LastName = sc.nextLine();
         System.out.println("Name is "+this.FirstName +" " + this.LastName );
-
+        System.out.println("Please select your graduation year:\n1: Junior\n2: Mid level\n3: Senior" );
+        this.graduateYear = sc.nextInt();
+        showInfo();
     }
 
     // Generate an student ID
+
+    public void setStudentID()
+    {
+        id++;
+
+    }
 
     // Enroll for courses
 
@@ -30,6 +39,11 @@ public class Student {
     // Pay tuition
 
     // Show status
+    public void showInfo()
+    {
+        System.out.println("Name "+this.FirstName + " " + this.LastName +
+                "\nGraduation Level : " + this.graduateYear);
+    }
 
 
 }
